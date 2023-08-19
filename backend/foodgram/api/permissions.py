b2747559e88261f80f7b0638b0ea16c7b19324ac
atsterq <atsterq@gmail.com>
2023-08-19 21:44:40 +0500

    user fix

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -23,12 +23,12 @@ class AuthUser(permissions.BasePermission):
 class Admin(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and (
-            request.user.is_admin or request.user.is_superuser
+            request.user.is_staff or request.user.is_superuser
         )
 
     def has_object_permission(self, request, view, obj):
         return request.user.is_authenticated and (
-            request.user.is_admin or request.user.is_superuser
+            request.user.is_staff or request.user.is_superuser
         )
 
 
