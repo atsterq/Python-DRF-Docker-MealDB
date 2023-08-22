df2eea0189ba2219dd75a6befdda1ceb558ca120
atsterq <atsterq@gmail.com>
2023-08-22 23:37:59 +0500

    building for deploy

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -109,9 +109,10 @@ USE_TZ = True
 
 
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "collected_static"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
